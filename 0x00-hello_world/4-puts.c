204d306653faea2c57f53da9cfa0beca113efcbe
Tadele21 <tade23dream@gmail.com>
2022-12-08 16:22:58 -0800

    ta

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
-/**
- * main - Entry
- * Return: Always 0
- */
-int main(void)
-{
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
-}
+#include <stdio.h>                                                         
+/**                                                                        
+ * main - Entry Point                                                 
+ * Return: Always 0 (Success)                                            
+ */                                                                   
+
+int main(void)                                                             
+{                                                                          
+     puts("\"Programming is like building a mutilingual puzzle");       
+     return (0);                                                        
+} 
